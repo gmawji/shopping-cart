4c1eea3fe60bad070b8c00bde8e036edf530e317
jokerg7 <git@gmawji.com>
2016-12-29 12:32:22 -0500

    Added escaping. Added Shopping Cart functionality.

--- main.py
+++ main.py
@@ -20,31 +20,8 @@ import jinja2
 import webapp2
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
-
-form_html = """
-<form>
-    <h2>Add a Food</h2>
-        <input type="text" name="food">
-        %s
-        <button>Add</button>
-</form>
-"""
-
-hidden_html = """
-<input type="hidden" name="food" value="%s">
-"""
-
-item_html = "<li>%s</li>"
-
-shopping_list_html = """
-<br>
-<br>
-<h2> Shopping List</h2>
-<ul>
-%s
-</ul>
-"""
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                                        autoescape = True)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -59,24 +36,8 @@ class Handler(webapp2.RequestHandler):
 
 class MainPage(Handler):
     def get(self):
-        self.render("shopping_list.html")
-
-        #output = form_html
-        #output_hidden = ""
-
-        #items = self.request.get_all("food")
-        #if items:
-        #    output_items = ""
-        #    for item in items:
-        #            output_hidden += hidden_html % item
-        #            output_items += item_html % item
-
-        #    output_shopping = shopping_list_html % output_items
-        #    output += output_shopping
-
-        #output = output % output_hidden
-
-        #self.write(output)
+        items = self.request.get_all("food")
+        self.render("shopping_list.html", items = items)
 
 class FizzBuzzHandler(Handler):
     def get(self):
